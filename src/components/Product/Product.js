ee996045a8ce0120b61ceaf71f0e91b77dc8dba4
rirefat <rirefat.official@gmail.com>
2022-03-22 23:25:23 +0600

    Add Button Position

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,18 +5,20 @@ import './Product.css'
 const Product = (props) => {
     const {id,title, image,description, category, rating,price} = props.product;
     const finalDescription = description.slice(0, 60);
+    const showedTitle = title.slice(0,20);
    
     return (
         <div className='single-product'>
             <img src={image} alt="" />
-            <h2>{title}</h2>
-            {/* <h2>{()=>loadMore(title)}</h2> */}
+            <h2>{showedTitle}... <span className='load-more-btn' onClick={()=>loadMore(title)}>FULL TITLE</span></h2>
             <p><strong>Description: </strong>{finalDescription}.... <strong onClick={()=>loadMore(description)} className="load-more-btn">Know More</strong></p>
             <p><strong>Category: </strong>{category}</p>
             <p><strong>Rating: </strong>{rating.rate}</p>
             <h3>Price: ${price}</h3>
-            <button onClick={()=>addToCart(id)}>Add To Cart</button>
-            <button onClick={()=>removeFromCart(id)}>Remove From Cart</button>
+            <div className="btn-area">
+                <button onClick={()=>addToCart(id)}>Add To Cart</button>
+                <button onClick={()=>removeFromCart(id)}>Remove From Cart</button>
+            </div>
         </div>
     );
 };
--- src/components/Utilities/functions.js
+++ src/components/Utilities/functions.js
@@ -49,28 +49,4 @@ const removeFromCart = id =>{
 }
 
 
-//===================================================== Practice Part =====================================================
-// const addToCart = (id) => {
-//     let shoppingCart;
-//     const storedCart = localStorage.getItem('shopping-cart');
-//     if(storedCart){
-//         shoppingCart = JSON.parse(storedCart);
-//     }
-//     else{
-//         shoppingCart={};
-//     }
-
-//     const qty = shoppingCart[id];
-//     if(qty){
-//         const newQty = qty+1;
-//         shoppingCart[id]=newQty;
-//     }
-//     else{
-//         shoppingCart[id] =1;
-//     }
-//     localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
-
-// }
-
-
 export {loadMore,addToCart, removeFromCart}
